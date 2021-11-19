94b06f7e833f31a1eeeebce94859962d5816925b
sanajylopis25 <sanajylopis25@gmail.com>
2021-11-19 18:43:02 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace learn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to learn");
+        }
+    }
+}
